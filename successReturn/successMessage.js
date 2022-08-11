27dd9abbb6bc124e6f50a0a273680ce1a6429a61
FernandaElizaiBeautty <111086413+FernandaElizaiBeautty@users.noreply.github.com>
2022-08-11 18:01:04 -0300

    Update successMessage.js

--- successReturn/successMessage.js
+++ successReturn/successMessage.js
@@ -1,6 +1,6 @@
 module.exports = function() {
-    this.inviteCodeSuccessMessage =  "Vinculo iBY Friends realizado com sucesso";
-    this.changeFirstPasswordSuccess = "Senha atualizada com sucesso";
+    this.inviteCodeSuccessMessage =  "Vínculo iBY Friends realizado com sucesso.";
+    this.changeFirstPasswordSuccess = "Senha atualizada com sucesso.";
     this.verifyCode = "Código verificado com sucesso.";
-    this.createCode = "Código criado com sucesso";
+    this.createCode = "Código criado com sucesso.";
 };
